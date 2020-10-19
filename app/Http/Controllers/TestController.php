bcf5051be0f37a04a1e1db171d629b4adc1fef02
T-8 <sundaradotto@gmail.com>
2020-10-19 16:09:24 +0900

    バリデーションの追加 エラーメッセージを日本語化

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Value;
+use App\Http\Requests\StoreForm;
 
 class TestController extends Controller
 {
@@ -37,7 +38,7 @@ class TestController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreForm $request)
     {
         //
         $test = new Value;
